908cc2a0730efc3438ce38ddd7957a9545c39d7d
krowter <krishna.rowter@gmail.com>
2021-04-21 20:44:43 +0700

    setup mongodb

--- src/server.js
+++ src/server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+
+import "regenerator-runtime/runtime.js";
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || "0.0.0.0";
 
@@ -9,34 +12,49 @@ import MessengerBotRoutes from "./routes/messengerBot";
 
 const app = express();
 const http = require("http").Server(app);
+const MongoClient = require("mongodb").MongoClient;
 
-// serve supporting css and js file
-app.use(express.static("src/client"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const uri = process.env.MONGODB_CONNECTION_STRING;
 
-// middleware to allow access to database
-app.use((req, {}, next) => {
-  req.db = db;
-  next();
-});
+MongoClient.connect(
+  uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err, client) => {
+    const db = client.db("cluster0");
 
-// routes
-app.use("/messages", MessageRoutes);
-app.use("/webhook", MessengerBotRoutes);
+    // parse incoming requests
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-const requestListener = function (req, res) {
-  //serve the app on root path
-  if (req.url === "/") req.url = "/index.html";
-  //serve supporting static files
-  fs.readFile(__dirname + "/client/index.html", (err, data) => {
-    res.writeHead(200);
-    res.end(data);
-  });
-};
+    // middleware to allow access to database
+    app.use((req, {}, next) => {
+      req.db = db;
+      next();
+    });
 
-app.get("/", requestListener);
+    // routes
+    app.use("/messages", MessageRoutes);
+    app.use("/webhook", MessengerBotRoutes);
 
-http.listen(PORT, () => {
-  console.log(`Server is running on http://${HOST}:${PORT}`);
-});
+    // serve supporting css and js file
+    app.use(express.static("src/client"));
+
+    const requestListener = function (req, res) {
+      //serve the app on root path
+      if (req.url === "/") req.url = "/index.html";
+      fs.readFile(__dirname + "/client/index.html", (err, data) => {
+        res.writeHead(200);
+        res.end(data);
+      });
+    };
+
+    app.get("/", requestListener);
+
+    http.listen(PORT, () => {
+      console.log(`Server is running on http://${HOST}:${PORT}`);
+    });
+  }
+);
